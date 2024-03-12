cf557dafb6349238d68db6f1325ed04525465de4
ykdman <dbsrudejr300@gmail.com>
2024-03-13 00:13:51 +0900

    useState 복습

--- react-final-countdown-app/src/components/Player.jsx
+++ react-final-countdown-app/src/components/Player.jsx
@@ -2,17 +2,22 @@ import { useState } from "react";
 
 export default function Player() {
   const [playerName, setPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
   function handleNameChange(event) {
-    console.log(event.target.value);
+    setSubmitted(false);
     setPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
   }
 
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : "unknown entity"}</h2>
       <p>
         <input type="text" onChange={handleNameChange} value={playerName} />
-        <button>Set Name</button>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
