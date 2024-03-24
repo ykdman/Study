d53d1db6d7e3cb25f72eac391e8061dfb034773e
ykdman <dbsrudejr300@gmail.com>
2024-03-24 23:47:46 +0900

    learn react useEffect, useCallback

--- react-side-effect/src/components/DeleteConfirmation.jsx
+++ react-side-effect/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
+const TIMER = 3000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    console.log("Timer SET");
+    const closeModalTimer = setTimeout(() => {
+      onConfirm();
+    }, TIMER);
+
+    return () => {
+      console.log("Cleaning Up Timer");
+      clearTimeout(closeModalTimer);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -11,6 +26,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
