faa5a9cc1a020cd7619ab0e490130ec206b69bd0
ykdman <dbsrudejr300@gmail.com>
2024-04-03 00:14:36 +0900

    create useFetch Cutom Hooks

---
+++ react-cutomhook/src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(fetchFn) {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetcheData] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetcheData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch user places." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, [fetchFn]);
+
+  return {
+    isFetching,
+    fetchedData,
+    error,
+  };
+}
