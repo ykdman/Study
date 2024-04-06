35d34da3866cca1c62673fc6e92173d372667da6
YKDman <dbsrudejr300@gmail.com>
2024-04-06 16:36:57 +0900

    Login.jsx 확인

--- react-form/src/components/Login.jsx
+++ react-form/src/components/Login.jsx
@@ -1,17 +1,47 @@
+// TODO : State 하나로 Email, Password, 값 받기
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+
+  function handleEmailChange(event) {
+    setEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setEnteredPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log('submitted');
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+          />
         </div>
       </div>
 
