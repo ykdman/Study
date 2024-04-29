42482f05baf9e066b6233f549e6e05c1fbdcd5b5
YKDman <dbsrudejr300@gmail.com>
2024-04-30 00:08:30 +0900

    feture : showCounter toggle

--- react-redux/src/store/index.js
+++ react-redux/src/store/index.js
@@ -1,22 +1,34 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "increment": {
+    case 'increment': {
       return {
         counter: state.counter + 1,
+        showCounter: state.showCounter,
       };
     }
-    case "increase": {
+    case 'increase': {
       const { step } = action;
       return {
         counter: state.counter + +step,
+        showCounter: state.showCounter,
       };
     }
 
-    case "decrement": {
+    case 'showcounter': {
+      return {
+        ...state,
+        showCounter: !state.showCounter,
+      };
+    }
+
+    case 'decrement': {
       return {
         counter: state.counter - 1,
+        showCounter: state.showCounter,
       };
     }
     default: {
