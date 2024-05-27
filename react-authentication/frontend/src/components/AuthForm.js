0839683bce31a34eb1dab0d99bc46eab8e98a3a7
YKDman <dbsrudejr300@gmail.com>
2024-05-27 17:37:22 +0900

    feature : 사용자로그인, 토큰 발행, 이미지 제거,추가,수정 기능

--- react-authentication/frontend/src/components/AuthForm.js
+++ react-authentication/frontend/src/components/AuthForm.js
@@ -1,19 +1,36 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import {
+  Form,
+  Link,
+  useSearchParams,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+  // action으로 발생한 데이터 사용
+  const actionData = useActionData();
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  const naviagation = useNavigation();
+
+  // url query 가져오기
+  const [searchParams] = useSearchParams();
+  const isLogin = searchParams.get("mode") === "login";
+  const isSubmitting = naviagation.state === "submitting";
 
   return (
     <>
       <Form method="post" className={classes.form}>
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {actionData && actionData.errors && (
+          <ul>
+            {Object.values(actionData.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {actionData && actionData.message && <p>{actionData.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -23,10 +40,12 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
-            {isLogin ? 'Create new user' : 'Login'}
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+            {isLogin ? "Create new user" : "Login"}
+          </Link>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Save"}
           </button>
-          <button>Save</button>
         </div>
       </Form>
     </>
--- react-authentication/frontend/src/pages/Authentication.js
+++ react-authentication/frontend/src/pages/Authentication.js
@@ -1,7 +1,47 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  // searchParams를 기본기능으로 구현
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (!mode === "signup" && !mode === "login") {
+    throw json({ message: "지원하지 않는 모드입니다." }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "사용자를 인증할 수 없습니다." }, { status: 500 });
+  }
+
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
+
+  return redirect("/");
+}
