3fe0edfa406f8dd4c3d6d8fc853ee475ddfed086
YKDman <dbsrudejr300@gmail.com>
2024-06-01 23:40:19 +0900

    react-query 쿼리 활성화 비활성화

--- react-tanstack/src/components/Events/FindEventSection.jsx
+++ react-tanstack/src/components/Events/FindEventSection.jsx
@@ -1,10 +1,48 @@
-import { useRef } from 'react';
+import { useQuery } from "@tanstack/react-query";
+import { useRef, useState } from "react";
+import { fetchEvents } from "../../util/http";
+import LoadingIndicator from "../UI/LoadingIndicator";
+import ErrorBlock from "../UI/ErrorBlock";
+import EventItem from "./EventItem";
 
 export default function FindEventSection() {
+  const [searchTerm, setSearchTerm] = useState(undefined);
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["events", { search: searchTerm }],
+    queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+    enabled: searchTerm !== undefined,
+  });
+
   const searchElement = useRef();
+
+  let content = <p>Please enter a search term and to find events</p>;
+
+  if (isLoading) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock message={error.info?.message} title="An error Occurred" />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="event-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
   }
 
   return (
@@ -20,7 +58,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
